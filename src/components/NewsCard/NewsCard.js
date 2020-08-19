68b0832ae898a4e38c9455f0a377426be0ace684
MD ARIF HOSSAIN <arif.swfu@outlook.com>
2020-08-20 02:40:04 +0800

    home page options cards

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -3,24 +3,27 @@ import React from 'react';
 import { Card, CardActions, CardActionArea, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
 
 // custom
-import newsImage from '../../img/news.jpg'
+import useStyles from './stylesNewsCard';
+import newsImage from '../../img/news.jpg';
 
 const NewsCard = ({ article: { description, publishedAt, source, title, url, urlToImage}, index }) => {
+    const classes = useStyles();
+
     return (
-        <Card>
-            <CardActionArea>
-                <CardMedia image={urlToImage || newsImage} />
-                <div>
+        <Card className={classes.card}>
+            <CardActionArea href={url} target="_blank">
+                <CardMedia className={classes.media} image={urlToImage || newsImage} />
+                <div className={classes.details}>
                     <Typography variant="body2" color="textSecondary" component="h2">{(new Date(publishedAt)).toDateString()}</Typography>
                     <Typography variant="body2" color="textSecondary" component="h2">{source.name}</Typography>
                 </div>
-                <Typography gutterBottom variant="h5">{title}</Typography>
+                <Typography className={classes.title} gutterBottom variant="h5">{title}</Typography>
                 <CardContent>
                     <Typography variant="body2" color="textSecondary" component="p">{description}</Typography>
                 </CardContent>
             </CardActionArea>
 
-            <CardActions>
+            <CardActions className={classes.cardActions}>
                 <Button size="small" color="primary">Learn More</Button>
                 <Typography variant="h5" color="textSecondary">{index + 1}</Typography>
             </CardActions>
