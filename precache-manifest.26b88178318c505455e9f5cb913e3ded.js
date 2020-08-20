35687b703f00c48fb5802417ddb7730ae71501e7
MD ARIF HOSSAIN <arif.swfu@outlook.com>
2020-08-20 16:21:06 +0800

    Updates

---
+++ precache-manifest.26b88178318c505455e9f5cb913e3ded.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8056e0a43fd78dc149a2920dc752e607",
+    "url": "/react-alan_ai-news_application/index.html"
+  },
+  {
+    "revision": "4a33cf4429d167f0dabd",
+    "url": "/react-alan_ai-news_application/static/css/main.89e15185.chunk.css"
+  },
+  {
+    "revision": "953a0f1f4e2406b3d4b6",
+    "url": "/react-alan_ai-news_application/static/js/2.090d239d.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/react-alan_ai-news_application/static/js/2.090d239d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a33cf4429d167f0dabd",
+    "url": "/react-alan_ai-news_application/static/js/main.a4f18544.chunk.js"
+  },
+  {
+    "revision": "2dd0995d0a61580af4d4",
+    "url": "/react-alan_ai-news_application/static/js/runtime-main.54a5cba3.js"
+  },
+  {
+    "revision": "c437c69452a05f9f65ae5969c83330c6",
+    "url": "/react-alan_ai-news_application/static/media/news.c437c694.jpg"
+  }
+]);
